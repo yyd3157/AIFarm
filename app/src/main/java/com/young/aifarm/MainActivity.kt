10ec23d2aa492a5d0d6cb26300c25260ff7d7264
yyd3157 <yyd3157@naver.com>
2024-04-08 19:28:56 +0900

    feat: 로그인시 Navigation 구현

--- app/src/main/java/com/young/aifarm/MainActivity.kt
+++ app/src/main/java/com/young/aifarm/MainActivity.kt
@@ -3,44 +3,31 @@ package com.young.aifarm
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.young.aifarm.ui.navigation.Navigation
 import com.young.aifarm.ui.theme.AIFarmTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AIFarmTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MyApp()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp() {
+    val navController = rememberNavController()
+    AIFarmTheme {
+        Navigation(navController)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    AIFarmTheme {
-        Greeting("Android")
-    }
+        MyApp()
 }
