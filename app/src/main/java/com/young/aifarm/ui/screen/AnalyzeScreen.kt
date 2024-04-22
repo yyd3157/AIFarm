cc31d1591cfcdb7612fe019778511a92d050c1e8
yyd3157 <yyd3157@naver.com>
2024-04-22 15:48:19 +0900

    feat: 앱 주요 Screen 추가

---
+++ app/src/main/java/com/young/aifarm/ui/screen/AnalyzeScreen.kt
@@ -0,0 +1,16 @@
+package com.young.aifarm.ui.screen
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun AnalyzeScreen() {
+    Box(modifier = Modifier
+        .fillMaxSize()
+    ) {
+        Text(text = "analyze")
+    }
+}
